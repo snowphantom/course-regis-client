249bc0bf2fb7827b35442f3fa3cc084f7651c32f
Hung Nguyen <snowphantom08@gmail.com>
2021-01-10 12:15:16 +0700

    udpate dist for server

---
+++ server.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const favicon = require('serve-favicon');
+const app = express();
+
+const forceSSL = function () {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+        ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+};
+app.use(express.static('./dist/course-regis-client'));
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname,'/dist/course-regis-client/index.html'));
+});
+
+app.use(forceSSL());
+
+app.listen(process.env.PORT || 8080);
